eb8515c84b2f2afbeca6849e1e6a765f37ab3fce
TylorJB <tylorbennett18@gmail.com>
2025-05-15 14:49:15 +1200

    styling: Added a backround image; this is a photo I took from the roof of the polytech

--- fullstack/src/app/about/page.jsx
+++ fullstack/src/app/about/page.jsx
@@ -8,11 +8,11 @@ export default function AboutPage() {
     <div
       className="relative flex flex-col items-center justify-center min-h-screen bg-gradient-to-br from-[#1E1B47] to-[#2F2C5D] text-white font-sans bg-cover bg-center"
       style={{
-        backgroundImage: "url('https://th.bing.com/th/id/R.54c46e9ebb362130c97b159fb5867c18?rik=Nol6XSQI7bvaJw&riu=http%3a%2f%2fopmarketing-prod.imgix.net%2fassets%2fAbout%2fOP-Quad-1200X610.jpg&ehk=yNgljmJXg24yKfLbCJeF5pi0Gs2AnnUT5flBhjCQrv0%3d&risl=&pid=ImgRaw&r=0')",
+        backgroundImage: `url("/images/background-aboutpage.jpg")`,
       }}
     >
 
-      <div className="absolute inset-0 bg-blue-500/30 backdrop-blur-lg z-0"></div>
+      {/* <div className="absolute inset-0 bg-blue-500/30 backdrop-blur-lg z-0"></div> */}
 
 
       <div className="z-10 w-full">
@@ -20,6 +20,9 @@ export default function AboutPage() {
       </div>
 
       <main className="z-10 flex flex-col items-center justify-center flex-1 p-6">
+        <section>
+          
+        </section>
         <div className="border-2 border-black bg-white text-black rounded-xl p-8 text-center max-w-md shadow-lg">
           <h1 className="text-2xl font-bold mb-4">🚧 About In Maintenance</h1>
           <p className="mb-6">Please return later.</p>
